8815d0c060396cf093a0b0dc0be50ec8581aee91
Cristiano S. Oliveira <cristiano.oliveira@venturus.org.br>
2025-06-01 22:06:37 -0300

    formats in generate primes

--- node/intensive-cpu/calc.js
+++ node/intensive-cpu/calc.js
@@ -2,7 +2,12 @@ import { workerData, parentPort } from "node:worker_threads";
 import generatePrimes from "./prime_generator.js";
 
 
-const primes = generatePrimes(workerData.count, workerData.start);
+const primes = generatePrimes(
+  workerData.count, 
+  workerData.start, {
+    format:true
+  }
+);
 
 parentPort.postMessage(primes)
 
